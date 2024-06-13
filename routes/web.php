5f7f2fbc8871619f6d206149af6659b8d77d693a
youngwiez <rakaawisesa@gmail.com>
2024-06-13 12:15:00 +0700

    Add Auth Login & Register

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,23 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\BarangMasukController;
 use App\Http\Controllers\BarangKeluarController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
 });
 
-Route::resource('kategori', KategoriController::class);
-Route::resource('barang', BarangController::class);
-Route::resource('barangmasuk', BarangMasukController::class);
-Route::resource('barangkeluar', BarangKeluarController::class);
+Route::get('login', [LoginController::class,'index'])->name('login')->middleware('guest');
+Route::post('login', [LoginController::class,'authenticate']);
+
+Route::get('logout', [LoginController::class,'logout']);
+Route::post('logout', [LoginController::class,'logout']);
+
+Route::post('register', [RegisterController::class,'store']);
+Route::get('register', [RegisterController::class,'create']);
+
+Route::resource('kategori', KategoriController::class)->middleware('auth');
+Route::resource('barang', BarangController::class)->middleware('auth');
+Route::resource('barangmasuk', BarangMasukController::class)->middleware('auth');
+Route::resource('barangkeluar', BarangKeluarController::class)->middleware('auth');
